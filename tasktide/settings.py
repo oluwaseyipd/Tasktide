e1e6fac460ff5320e966e6358d2432d3b8f9fe1f
Oluwaseyi <oluwaseyipd@gmail.com>
2025-08-27 12:12:29 +0100

    Updated requirement.txt for production

--- tasktide/settings.py
+++ tasktide/settings.py
@@ -16,8 +16,6 @@ import os
 
 from pathlib import Path
 
-from decouple import config
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
