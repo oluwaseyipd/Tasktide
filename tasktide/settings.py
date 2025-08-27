8a8110b667a11a94c8fe75ec64e6b1e6fc083e91
Oluwaseyi <oluwaseyipd@gmail.com>
2025-08-27 12:16:26 +0100

    Set DEBUG in settings.py for production

--- tasktide/settings.py
+++ tasktide/settings.py
@@ -16,6 +16,11 @@ import os
 
 from pathlib import Path
 
+from decouple import config
+
+
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
