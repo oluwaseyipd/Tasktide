37e11eae566bbdc20fd95b043539531c75962ed8
Oluwaseyi <oluwaseyipd@gmail.com>
2025-12-22 16:43:01 +0100

    Add setuptools to requirements for drf_yasg compatibility

--- tasktide/settings.py
+++ tasktide/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False, cast=bool)
+DEBUG = config("DEBUG", default=True, cast=bool)
 
 # Updated ALLOWED_HOSTS for Render
 
